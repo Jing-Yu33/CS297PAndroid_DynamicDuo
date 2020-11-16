5a450423a19353f8b6eedcb852c0f9eba1999322
MoonLight <yuj19@uci.edu>
2020-11-15 22:51:14 -0800

    feat: add login/signup page

---
+++ app/src/main/java/com/example/cs297pandroid_dynamicduo/LogInActivity.java
@@ -0,0 +1,18 @@
+package com.example.cs297pandroid_dynamicduo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class LogInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in);
+    }
+    public void logIn(View view) {
+
+    }
+}
---
+++ app/src/main/java/com/example/cs297pandroid_dynamicduo/SignUpActivity.java
@@ -0,0 +1,18 @@
+package com.example.cs297pandroid_dynamicduo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up_activity);
+    }
+    public void signUp(View view) {
+
+    }
+}
