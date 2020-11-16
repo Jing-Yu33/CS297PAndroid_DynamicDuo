a5389d18669c24dc76ced4b33bcc0830176c9411
MoonLight <yuj19@uci.edu>
2020-11-16 12:09:50 -0800

    fix: add build.bundle

---
+++ app/src/main/java/com/example/cs297pandroid_dynamicduo/SignUp/PolicyActivity.java
@@ -0,0 +1,15 @@
+package com.example.cs297pandroid_dynamicduo.SignUp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.cs297pandroid_dynamicduo.R;
+public class PolicyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_policy);
+    }
+}
